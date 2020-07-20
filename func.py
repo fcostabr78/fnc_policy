d81e05db73297f074ca3eb5632f7824428cd1797
Fernando <fdacosta1978@gmail.com>
2020-07-20 19:08:38 -0300

    Zero Quota created

--- func.py
+++ func.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from random import randint
 from oci.identity.models import CreatePolicyDetails
 from oci.object_storage.models import CreateBucketDetails
+from oci.limits import QuotasClient
+from oci.limits.models import CreateQuotaDetails, UpdateQuotaDetails
 from fdk import response
 
 compartment_id = 'ocid1.tenancy.oc1..aaaaaaaaqqzek25x6oc72fsf7pl5pxqipakzcual27u6db3njlq76p7jopna'
@@ -27,6 +29,22 @@ def save_log(signer, body):
     object_name = 'log' + ts + str(randint(0, 10000))
     object_storage.put_object(namespace, bucket_name, object_name, my_data)
 
+def zero_quota(signer, name, description):
+    try:
+        quotas_client = QuotasClient(config={}, signer=signer)
+        statements = ["zero compute-core quota in tenancy", "zero block-storage quota in tenancy", "zero filesystem quota in tenancy", "zero database quota in tenancy"]
+        new_quota = CreateQuotaDetails()
+        new_quota.compartment_id = compartment_id
+        new_quota.name = name
+        new_quota.description = description
+        new_quota.statements = statements
+        quotas_client.create_quota(new_quota).data
+        resp = { 'quota': 'Zerada com sucesso para compute, block-storage, database e filesystem'}
+    except Exception as ex:
+        resp = { 'quota': 'Ocorreu um erro: {}'.format(ex) }
+
+    return resp
+
 def create_policy(signer):
     identity = oci.identity.IdentityClient(config={}, signer=signer)
     try:
@@ -45,7 +63,9 @@ def create_policy(signer):
 
 def handler(ctx, data: io.BytesIO=None):
     signer = oci.auth.signers.get_resource_principals_signer()
-    resp = create_policy(signer)
+    resp_policy = create_policy(signer)
+    resp_quota = zero_quota(signer, 'new_quota', 'testing')
+    resp = {'policies' : resp_policy, 'quotas' : resp_quota}
     save_log(signer, resp)
     return response.Response(
         ctx, response_data=json.dumps(resp), headers={"Content-Type": "application/json"}
